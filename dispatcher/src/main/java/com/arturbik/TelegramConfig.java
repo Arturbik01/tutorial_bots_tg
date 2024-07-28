0586cf7c6b1c7a2be6646283da511e021fdadd46
Arturbik <onlinitan@gmail.com>
2024-07-28 23:07:33 +0700

    up tg bot on lpool

---
+++ dispatcher/src/main/java/com/arturbik/TelegramConfig.java
@@ -0,0 +1,34 @@
+package com.arturbik;
+
+import com.arturbik.controller.TelegramBot;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+@PropertySource("classpath:application.yaml")
+@Data
+public class TelegramConfig {
+
+    @Value("${bot.telegram.token}")
+    private String botToken;
+
+    @Value("${bot.telegram.username}")
+    private String botUsername;
+
+
+    @Bean
+    public TelegramBot getTelegramBot() throws TelegramApiException {
+        TelegramBot telegramBot = new TelegramBot(getBotToken(), getBotUsername());
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+
+        api.registerBot(telegramBot);
+        return telegramBot;
+    }
+
+}
---
+++ dispatcher/src/main/java/com/arturbik/controller/TelegramBot.java
@@ -0,0 +1,35 @@
+package com.arturbik.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+@RequiredArgsConstructor
+@Log4j
+public class TelegramBot extends TelegramLongPollingBot {
+    String botUsername;
+
+
+    public TelegramBot(String botToken, String botUsername) {
+        super(botToken);
+        this.botUsername = botUsername;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        log.debug(update.getMessage().getText());
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+
+
+}
